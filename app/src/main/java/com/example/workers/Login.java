8886994918f65c630dc39f525fa8ee3ad9d92b7e
rajathirumal <rajathirumal98@gmail.com>
2020-04-11 17:41:20 +0530

    Sign up page UI designed.

--- app/src/main/java/com/example/workers/Login.java
+++ app/src/main/java/com/example/workers/Login.java
@@ -2,7 +2,9 @@ package com.example.workers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+
+
+
+    public void onRegisterClick(View v){
+        Intent moveToSignUp = new Intent(Login.this,SignUp.class);
+        startActivity(moveToSignUp);
     }
 }
--- app/src/main/java/com/example/workers/SignUp.java
+++ app/src/main/java/com/example/workers/SignUp.java
@@ -3,12 +3,33 @@ package com.example.workers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class SignUp extends AppCompatActivity {
+
+    private Spinner categorySelectorSpinner;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        categorySelectorSpinner = findViewById(R.id.categorySpinner);
+        signUpBtn = findViewById(R.id.btnSignUp);
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "onClick: "+ String.valueOf(categorySelectorSpinner.getSelectedItem()) );
+            }
+        });
     }
+
+
+
 }
