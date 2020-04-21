710d8ef3a2a8339468933ede4a573cef5f42bf22
rajathirumal <rajathirumal98@gmail.com>
2020-04-21 21:46:28 +0530

    Worker dashBoard updated.

--- app/src/main/java/com/example/workers/WorkerDashBoard.java
+++ app/src/main/java/com/example/workers/WorkerDashBoard.java
@@ -1,7 +1,11 @@
 package com.example.workers;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,27 +13,40 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class WorkerDashBoard extends AppCompatActivity {
-    private Button logOutWorker;
     private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_dash_board);
-
-        logOutWorker = findViewById(R.id.btnLogOutWorker);
         firebaseAuth = FirebaseAuth.getInstance();
-
-
-        logOutWorker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logoutWorker();
-            }
-        });
     }
 
-    private void logoutWorker() {
-        firebaseAuth.signOut();
-        finish();
+
+    public void onSettingsClickedByWorker(View view) {
+        startActivity(new Intent(WorkerDashBoard.this,Worker_setting.class));
+    }
+    public  void onLogOutClickedByWorker(View view){
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(WorkerDashBoard.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(WorkerDashBoard.this);
+        }
+        builder.setTitle("Delete Entry")
+                .setMessage("Are you sure you want to log-out ?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        firebaseAuth.signOut();
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
---
+++ app/src/main/java/com/example/workers/Worker_setting.java
@@ -0,0 +1,31 @@
+package com.example.workers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Worker_setting extends AppCompatActivity {
+    private Button logOutWorker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_setting);
+
+
+
+
+
+    }
+
+    private void logoutWorker() {
+
+    }
+
+
+}
